c44915bf5405ad5bc461ccebc530dcd84852effb
Tania A <taniaallen@Tanias-MacBook-Pro.local>
2016-05-16 02:19:31 -0400

    added functions for game, unfinished

--- starter-code/main.js
+++ starter-code/main.js
@@ -6,6 +6,12 @@ var cardThree = "king";
  
 var cardFour = "king";
 
+var cards = ['queen', 'queen', 'king', 'king'];
+var cardsInPlay = [];
+
+
+
+
 /* Commenting out if statement per Unit 10.7 assignment insructions.
 
 if (cardTwo === cardFour) {
@@ -21,11 +27,28 @@ var boardDiv = document.getElementById('game-board');
 
 
 var createBoard = function() {
-	for (var i = 0; i < 4; i++) {
+	for (var i = 0; i < cards.length; i++) {
 		var newDiv = document.createElement('div');
 	    newDiv.className = 'card';
 	    boardDiv.appendChild(newDiv);
+	    cardElement.setAttribute('data-card', cards[i]);
+	    cardElement.innerHTML = '<img src="Clubs 12.png" alt="King of Clubs" />';addEventListener('click', isTwoCards);
 	};
+}
+
+function isMatch() {
+	if(cardsInPlay[0].key === cardsInPlay[1].key) {
+
+	}
+}
+
+function isTwoCards() {
+  cardsInPlay.push(this.getAttribute('data-card'));
+  if (cardsInPlay.length === 2) {
+    isMatch(cardsInPlay);
+    cardsInPlay = [];
+  }
+
 }
 
 createBoard();

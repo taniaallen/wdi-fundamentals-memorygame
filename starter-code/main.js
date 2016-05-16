209e15990f2996db3a98140be55b5ea1289ead1d
Tania A <taniaallen@Tanias-MacBook-Pro.local>
2016-05-15 23:25:55 -0400

    added createBoard function

--- starter-code/main.js
+++ starter-code/main.js
@@ -6,8 +6,26 @@ var cardThree = "king";
  
 var cardFour = "king";
 
+/* Commenting out if statement per Unit 10.7 assignment insructions.
+
 if (cardTwo === cardFour) {
 	alert("You found a match!");
 } else {
 	alert("Sorry, try again.");
 }
+*/
+
+
+
+var boardDiv = document.getElementById('game-board');
+
+
+var createBoard = function() {
+	for (var i = 0; i < 4; i++) {
+		var newDiv = document.createElement('div');
+	    newDiv.className = 'card';
+	    boardDiv.appendChild(newDiv);
+	};
+}
+
+createBoard();
